0b3c6863a3dffa2efd5dbb43e4cdc4c70a73afb9
rajoshich <rajoshich@gmail.com>
2020-05-23 19:03:57 +0530

    doesn't crash

--- app/src/main/java/com/rajoshich/annoyingex/APIManager.kt
+++ app/src/main/java/com/rajoshich/annoyingex/APIManager.kt
@@ -1,6 +1,7 @@
 package com.rajoshich.annoyingex
 
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.RequestQueue
@@ -10,28 +11,38 @@ import com.google.gson.Gson
 import kotlin.random.Random
 
 class APIManager(context: Context) {
-    private lateinit var contentText: String
-    private val queue: RequestQueue = Volley.newRequestQueue(context)
-    private lateinit var listOfMessages: Messages
-
-    fun getMessages() {
-        val messagesURL = "https://raw.githubusercontent.com/echeeUW/codesnippets/master/ex_messages.json"
-        val req = StringRequest(
-            Request.Method.GET, messagesURL,
-            {
-                val gson = Gson()
-                listOfMessages = gson.fromJson(it, Messages::class.java)
-//                onMessagesReady(listOfMessages)
-//                val index = Random.nextInt(listOfMessages.messages.size)
-//                contentText = listOfMessages.messages[index]
-            },
-            {
-                contentText = "unable to retrieve message"
-                    })
-        queue.add(req)
-    }
-
-    fun returnMessages() :Messages {
-        return listOfMessages
-    }
+//private lateinit var contentText: String
+//    private val queue: RequestQueue = Volley.newRequestQueue(context)
+//
+//
+//    private fun getMessages():Messages {
+//        var listOfMessages: Messages
+//        val messagesURL =
+//            "https://raw.githubusercontent.com/echeeUW/codesnippets/master/ex_messages.json"
+//        val req = StringRequest(
+//            Request.Method.GET, messagesURL,
+//            {response ->
+//                val gson = Gson()
+//                listOfMessages = gson.fromJson(response, Messages::class.java)
+////                onMessagesReady(listOfMessages)
+//            },
+//            {
+//                Log.e("ERROR", it.networkResponse.toString())
+//            })
+//        queue.add(req)
+//    }
+//
+//
+//    fun returnMessages() :String {
+//        val list = getMessages()
+//        if (list != null) {
+//            val index = Random.nextInt(list.messages.size)
+//
+//            contentText = list.messages[index]
+//            return contentText
+//        } else {
+//            return ("unable to retrieve message")
+//
+//    }
+//    }
 }
--- app/src/main/java/com/rajoshich/annoyingex/MainActivity.kt
+++ app/src/main/java/com/rajoshich/annoyingex/MainActivity.kt
@@ -2,24 +2,27 @@ package com.rajoshich.annoyingex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    lateinit var apiManager: APIManager
+ //   lateinit var apiManager: APIManager
+    lateinit var contentText: String
+    lateinit var listOfMessages: Messages
+    var text = "unable to retrieve message"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val annoyApp = (application as AnnoyApp)
         val annoyNotifManager = annoyApp.annoyNotificationManager
-        apiManager = annoyApp.apiManager
-        val texts = apiManager.returnMessages()
-        val index = Random.nextInt(texts.messages.size)
-        if (texts == null) {
-            annoyNotifManager.contentText = "unable to retrieve message"
-        }
-        annoyNotifManager.contentText  = texts.messages[index]
+     //   apiManager = annoyApp.apiManager
 
 
         annoyButton.setOnClickListener {
@@ -33,6 +36,33 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+
+
+        val queue: RequestQueue = Volley.newRequestQueue(this)
+        fun getMessages() {
+            val messagesURL =
+                "https://raw.githubusercontent.com/echeeUW/codesnippets/master/ex_messages.json"
+            val req = StringRequest(
+                Request.Method.GET, messagesURL,
+                {response ->
+                    val gson = Gson()
+                    listOfMessages = gson.fromJson(response, Messages::class.java)
+//                onMessagesReady(listOfMessages)
+                },
+                {
+                    Log.e("ERROR", it.networkResponse.toString())
+                })
+            queue.add(req)
+        }
+
+
+        fun returnMessage() {
+            if (listOfMessages != null) {
+                val index = Random.nextInt(listOfMessages.messages.size)
+                text = listOfMessages.messages[index]
+            }
+        }
+        annoyNotifManager.contentText  = text
 
     }
 }
